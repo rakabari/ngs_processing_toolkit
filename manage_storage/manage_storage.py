b5d4aea1eae5381ef25c4766200237ce12684ca1
Ratilal Akabari <35432779+rakabari@users.noreply.github.com>
2024-12-27 11:26:04 -0500

    Update manage_storage.py

--- manage_storage/manage_storage.py
+++ manage_storage/manage_storage.py
@@ -11,18 +11,18 @@ from utils.mnt_win import mount_drive
 
 
 def mount_drives(mount_pints: List) -> None:
-    """Mounts S and MiSeq D Drives using mount_drive function"""
+    "Mounts S and MiSeq D Drives using mount_drive function"
     for mount_point in mount_pints:
         mount_drive(mount_point)
 
 
 def get_previously_transferred_runs(s_dir_list: List) -> List[str]:
-    """get completed runs from S drive Myeloid Illumina_Runs"""
+    "get completed runs from S drive Myeloid Illumina_Runs"
     return [rf.name for directory in s_dir_list for rf in os.scandir(directory)]
 
 
 def move_runs_to_s_drive(paths: list, s_dir_list: List) -> None:
-    """Move linux share runs to S Drive"""
+    "Move linux share runs to S Drive"
     # list all runs on ilinux share
     linux_runs = [rf.resolve()for path in paths for rf in Path(path).iterdir()]
     already_moved = get_previously_transferred_runs(s_dir_list)
@@ -40,7 +40,7 @@ def move_runs_to_s_drive(paths: list, s_dir_list: List) -> None:
 
 
 def remove_old_runs_from_miseqs(paths: list, s_dir_list: List) -> None:
-    """Remove old MiSeq runs from D drives if they are backed up on S Drive"""
+    "Remove old MiSeq runs from D drives if they are backed up on S Drive"
     # list all runs on MiSeqOutput/MiSeqAnalysis
     miseq = [rf.resolve()for path in paths for rf in Path(path).iterdir()]
     backed_up = get_previously_transferred_runs(s_dir_list)
@@ -53,7 +53,7 @@ def remove_old_runs_from_miseqs(paths: list, s_dir_list: List) -> None:
 
 
 def move_old_to_sub_dir(directory, sub_directory) -> None:
-    """Move old S Drive (MiSeqRuns) runs to S Drive Older_Runs"""
+    "Move old S Drive (MiSeqRuns) runs to S Drive Older_Runs"
     for rf in os.scandir(directory):
         rf = Path(rf)
         if is_old_run(rf, n=30):
